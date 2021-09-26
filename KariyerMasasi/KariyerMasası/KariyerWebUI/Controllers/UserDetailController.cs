9e74931a000d8bc13d2496d9e05ea7c3e4b01a94
kadir1278 <kadirdestek1@gmail.com>
2021-09-27 02:31:48 +0300

    userdetail veri ekleme controller yazıldı

--- KariyerMasasi/KariyerMasası/KariyerWebUI/Controllers/UserDetailController.cs
+++ KariyerMasasi/KariyerMasası/KariyerWebUI/Controllers/UserDetailController.cs
@@ -18,13 +18,9 @@ namespace KariyerWebUI.Controllers
         public ActionResult Index => View();
         public JsonResult GetUserData(int userID)
         {
-           List<User> data = new List<User>();
-
-            data = db.Users.Where(x => !x.DeletionStatus && x.ID == userID).ToList();
-
             var query = new
             {
-                Result = (from usr in data
+                Result = (from usr in db.Users.Where(x => !x.DeletionStatus && x.ID == userID)
                           join usrcertificate in db.UserCertificates.Where(x => !x.DeletionStatus && x.UserID == userID) on usr.ID equals usrcertificate.UserID
                           join usrcomputer in db.UserComputerInformations.Where(x => !x.DeletionStatus && x.UserID == userID) on usr.ID equals usrcomputer.UserID
                           join usrbusiness in db.UserBusinessInformations.Where(x => !x.DeletionStatus && x.UserID == userID) on usr.ID equals usrbusiness.UserID
@@ -32,14 +28,14 @@ namespace KariyerWebUI.Controllers
                           join usrseminar in db.UserReferences.Where(x => !x.DeletionStatus && x.UserID == userID) on usr.ID equals usrseminar.UserID
 
                           select new
-                         {
-                             ID = usr.ID,
-                             Name = usr.Name,
-                             Surname = usr.Surname,
-                             EMail = usr.EMail,
-                             Phone = usr.Phone,
-                             CertificateName=usrcertificate.CertificateName,
-                         })
+                          {
+                              ID = usr.ID,
+                              Name = usr.Name,
+                              Surname = usr.Surname,
+                              EMail = usr.EMail,
+                              Phone = usr.Phone,
+                              CertificateName = usrcertificate.CertificateName,
+                          }).ToList()
             };
 
             return Json(query, JsonRequestBehavior.AllowGet);
@@ -47,9 +43,9 @@ namespace KariyerWebUI.Controllers
 
 
         [HttpPost]
-        public ActionResult Add(UserDetailViewModel model, UserCertificate userCertificate, string CertificateFile)
+        public ActionResult Add(UserDetailViewModel model, UserCertificate userCertificate, UserSeminar userSeminar, UserComputerInformation userComputer, UserReference userReference, UserBusinessInformation userBusiness, string CertificateFile)
         {
-            if (model.Certificates != null)
+            if (model.Certificates.Count > 0)
             {
                 foreach (var item in model.Certificates)
                 {
@@ -71,7 +67,71 @@ namespace KariyerWebUI.Controllers
                     userCertificate.Status = item.Status;
                     userCertificate.LanguageID = item.LanguageID;
                     userCertificate.CertificateFinishDate = item.CertificateFinishDate;
+                    userCertificate.DeletionStatus = false;
+                    userCertificate.CreatedTime = DateTime.Now;
+                    userCertificate.UpdatedTime = DateTime.Now;
                     db.UserCertificates.Add(userCertificate);
+                    db.SaveChanges();
+                }
+            }
+            if (model.ComputerInformations.Count > 0)
+            {
+                foreach (var item in model.ComputerInformations)
+                {
+                    userComputer.UserID = model.UserID;
+                    userComputer.Name = item.Name;
+                    userComputer.Description = item.Description;
+                    userComputer.DeletionStatus = false;
+                    userComputer.CreatedTime = DateTime.Now;
+                    userComputer.UpdatedTime = DateTime.Now;
+                    db.UserComputerInformations.Add(userComputer);
+                    db.SaveChanges();
+                }
+            }
+            if (model.BusinessInformations.Count>0)
+            {
+                foreach (var item in model.BusinessInformations)
+                {
+                    userBusiness.UserID = model.UserID;
+                    userBusiness.CompanyName = item.CompanyName;
+                    userBusiness.StartingDate = item.StartingDate;
+                    userBusiness.FinishDate = item.FinishDate;
+                    userBusiness.Duty = item.Duty;
+                    userBusiness.Description = item.Description;
+                    userBusiness.DeletionStatus = false;
+                    userBusiness.CreatedTime = DateTime.Now;
+                    userBusiness.UpdatedTime = DateTime.Now;
+                    db.UserBusinessInformations.Add(userBusiness);
+                    db.SaveChanges();
+                }
+            }
+            if (model.References.Count>0)
+            {
+                foreach (var item in model.References)
+                {
+                    userReference.UserID = model.UserID;
+                    userReference.NameSurname = item.NameSurname;
+                    userReference.Phone = item.Phone;
+                    userReference.EMail = item.EMail;
+                    userReference.DeletionStatus = false;
+                    userReference.CreatedTime = DateTime.Now;
+                    userReference.UpdatedTime = DateTime.Now;
+                    db.UserReferences.Add(userReference);
+                    db.SaveChanges();
+                }
+            }
+            if (model.Seminars.Count>0)
+            {
+                foreach (var item in model.Seminars)
+                {
+                    userSeminar.UserID = model.UserID;
+                    userSeminar.Name = item.Name;
+                    userSeminar.SeminarDate = item.SeminarDate;
+                    userSeminar.Description = item.Description;
+                    userSeminar.DeletionStatus = false;
+                    userSeminar.CreatedTime = DateTime.Now;
+                    userSeminar.UpdatedTime = DateTime.Now;
+                    db.UserSeminars.Add(userSeminar);
                     db.SaveChanges();
                 }
             }
