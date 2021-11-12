d0c8baaadfd20989139c58f775b3d082512111e7
Kadir Ari <kadirdestek1@gmail.com>
2021-11-13 00:16:45 +0300

    kullanıcı profili hariç işlemler tamamlandı

---
+++ KariyerMasasi/KariyerMasası/KariyerWebUI/Content/js/pages/specialdirectories.js
@@ -0,0 +1,69 @@
+﻿async function GetSpecialData() {
+    var url = '/ozel-durum-getir';
+    $('#example').html("");
+    var thead =
+        '<thead class="thead-primary"><tr>' +
+        '<th class="sorting_asc">Kullanıcı Adı</th>' +
+        '<th class="sorting_asc">Özel Durum</th>' +
+        '</tr></thead>';
+    $('#example').append(thead);
+
+    $.getJSON(url, function (data) {
+        console.log(data)
+        for (item in data.Result) {
+            var tableData =
+                '<tbody><tr role="row"><div class="d-flex align-items-center">' +
+                '<td>' + data.Result[item].User + '</td>' +
+                '<td>' + data.Result[item].SpecialType + '</td>' +
+                '<td><div class="d-flex">' +
+                '<a id="btnDelete" style="color:white" class="btn btn-danger shadow btn-xs sharp" data-id="' + data.Result[item].ID + '"><i class="fa fa-trash"></i></a>' +
+                '</div></td></tr></tbody> '
+            $('#example').append(tableData);
+        };
+
+    })
+}
+$(document).ready(GetSpecialData());
+
+$("#example").on("click", "#btnDelete", function () {
+    var btn = $(this);
+    bootbox.confirm("Silmek istediğinize emin misiniz ?", function (result) {
+        if (result) {
+            var ID = btn.data("id");
+            $.ajax({
+                type: "GET",
+                url: "/ozel-durum-sil/" + ID,
+                success: function () {
+                    bootbox.alert("Özel durum silme işlemi başarılı.")
+                    GetSpecialData();
+                },
+                error: function () {
+                    bootbox.alert("Özel durum silinemedi lütfen tekrar deneyin. Sorunun devam etmesi durumunda 360MEKA ile irtibat kurunuz.");
+                }
+            })
+        }
+    })
+})
+
+$("#special-add-form").submit(function (e) {
+    e.preventDefault();
+    var btnClose = document.getElementById("closeUser");
+    var user = {
+        UserID: $("#special-add-form").find('[name="AddUserID"]').val(),
+        UserSpecialTypeID: $("#special-add-form").find('[name="AddUserSpecialTypeID"]').val(),
+
+    }
+    $.ajax({
+        type: "POST",
+        url: "/ozel-durum-ekle",
+        data: user,
+        success: function () {
+            bootbox.alert("Veri Eklendi")
+            GetSpecialData();
+            btnClose.click();
+        },
+        error: function () {
+            bootbox.alert("Eksik bilgileri doldurunuz")
+        }
+    });
+});
--- KariyerMasasi/KariyerMasası/KariyerWebUI/Controllers/SpecialDirectoryController.cs
+++ KariyerMasasi/KariyerMasası/KariyerWebUI/Controllers/SpecialDirectoryController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using KariyerWebUI.Models;
 
 namespace KariyerWebUI.Controllers
 {
@@ -14,7 +15,7 @@ namespace KariyerWebUI.Controllers
         private SystemContext db = new SystemContext();
         [Route("ozel-durum"),HttpGet]
         public ActionResult Index() => View();
-        [Route("GetSpecialDirectoryData"), HttpGet]
+        [Route("ozel-durum-getir"), HttpGet]
         public JsonResult GetSpecialDirectoryData()
         {
             List<SpecialDirectory> data = new List<SpecialDirectory>();
@@ -25,11 +26,36 @@ namespace KariyerWebUI.Controllers
                          select new
                          {
                              ID = obj.ID,
-                             UserID = obj.User.Name,
-                             UserSpecialTypeID = obj.UserSpecialType.Name,
+                             User = obj.User.Name,
+                             SpecialType= obj.UserSpecialType.Name,
                          }
             };
             return Json(query, JsonRequestBehavior.AllowGet);
+        }
+        [Route("ozel-durum-ekle"), HttpGet]
+        public ActionResult Add(SpecialDirectoryViewModel model)
+        {
+            model.Users = db.Users.Where(x => !x.DeletionStatus).ToList();
+            model.UserSpecialTypes = db.UserSpecialTypes.Where(x => !x.DeletionStatus).ToList();
+            return PartialView(model);
+        }
+        [Route("ozel-durum-ekle"),HttpPost]
+        public ActionResult AddSpecial(SpecialDirectory model)
+        {
+            model.CreatedTime = DateTime.Now;
+            model.UpdatedTime = DateTime.Now;
+            model.DeletionStatus = false;
+            db.SpecialDirectories.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [Route("ozel-durum-sil/{id}"),HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var data = db.SpecialDirectories.Find(id);
+            data.DeletionStatus = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
