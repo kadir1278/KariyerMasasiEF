4471641a23b5f1df2ac074caf27e3bdb912f294b
kadir1278 <kadirdestek1@gmail.com>
2021-11-15 14:46:51 +0300

    kullanıcı profil partial yapısı kuruldu

--- KariyerMasasi/KariyerMasası/KariyerWebUI/Controllers/UserProfileController.cs
+++ KariyerMasasi/KariyerMasası/KariyerWebUI/Controllers/UserProfileController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using System.Net;
+using KariyerEntity.Entity;
 
 namespace KariyerWebUI.Controllers
 {
@@ -16,18 +18,44 @@ namespace KariyerWebUI.Controllers
         public ActionResult Index(UserProfileViewModel model)
         {
             var loginUser = Session["loginUser"] as LoginViewModel;
-            int userID = loginUser.ID;
-            model.UserInformations= db.Users.Include(x=>x.UserRoles).Where(x => !x.DeletionStatus && x.ID == userID).ToList();
-            model.BusinessInformations = db.UserBusinessInformations.Where(x => !x.DeletionStatus && x.UserID == userID).ToList();
-            model.Certificates = db.UserCertificates.Where(x => !x.DeletionStatus && x.UserID == userID).ToList();
-            model.ComputerInformations = db.UserComputerInformations.Where(x => !x.DeletionStatus && x.UserID == userID).ToList();
-            model.Educations = db.UserEducations.Where(x => !x.DeletionStatus && x.UserID == userID).ToList();
-            model.Languages = db.UserLanguages.Where(x => !x.DeletionStatus&&x.UserID==userID).ToList();
-            model.References = db.UserReferences.Where(x => !x.DeletionStatus && x.UserID == userID).ToList();
-            model.Seminars = db.UserSeminars.Where(x => !x.DeletionStatus && x.UserID == userID).ToList();
-            model.SpecialDirectories = db.SpecialDirectories.Where(x => !x.DeletionStatus && x.UserID == userID).ToList();
+            if (loginUser == null)
+            {
+                Redirect("/");
+            }
+            else
+            {
+                int userID = loginUser.ID;
+                InModel(model, userID);
+                return View(model);
+            }
+            return View();
+        }
 
+
+        [Route("profil/{id}")]
+        [AllowAnonymous]
+        public ActionResult Index(UserProfileViewModel model, int id)
+        {
+            User about = db.Users.Where(x => !x.DeletionStatus && x.ID == id).FirstOrDefault();
+            if (about == null)
+            {
+                return HttpNotFound();
+            }
+            InModel(model, id);
             return View(model);
         }
+        private void InModel(UserProfileViewModel model, int userID)
+        {
+            model.UserInformations = db.Users.Include(x => x.BusinessArea).Where(x => !x.DeletionStatus && x.ID == userID).ToList(); //+
+            model.BusinessInformations = db.UserBusinessInformations.Where(x => !x.DeletionStatus && x.UserID == userID).ToList(); //+
+            model.Certificates = db.UserCertificates.Include(x => x.Language).Where(x => !x.DeletionStatus && x.UserID == userID).ToList(); //+
+            model.ComputerInformations = db.UserComputerInformations.Where(x => !x.DeletionStatus && x.UserID == userID).ToList(); //+
+            model.Educations = db.UserEducations.Where(x => !x.DeletionStatus && x.UserID == userID).ToList();// +
+            model.Languages = db.UserLanguages.Where(x => !x.DeletionStatus && x.UserID == userID).ToList();// +
+            model.References = db.UserReferences.Where(x => !x.DeletionStatus && x.UserID == userID).ToList(); //+
+            model.Seminars = db.UserSeminars.Where(x => !x.DeletionStatus && x.UserID == userID).ToList(); //+
+            model.SpecialDirectories = db.SpecialDirectories.Include(x=>x.UserSpecialType).Where(x => !x.DeletionStatus && x.UserID == userID).ToList();
+        }
+
     }
 }
---
+++ KariyerMasasi/KariyerMasası/KariyerWebUI/Controllers/UserSeminarController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerWebUI.Controllers
+{
+    public class UserSeminarController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("semienr-ekle"),HttpGet]
+        public ActionResult Add() => PartialView();
+    }
+}
