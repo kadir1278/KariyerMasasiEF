8bb6161d1900212d6477263ab984fa2862a0b500
Ermand Haruni <ermandgs74@gmail.com>
2021-11-28 21:58:50 +0300

    Company Register
    Şirket kaydı için statik sayfa hazırlandı

---
+++ KariyerMasasi/KariyerMasası/KariyerWebUI/Controllers/RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerWebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult UserRegister()
+        {
+            return View();
+        }
+
+        [Route("sirket-kayit"), HttpGet]
+        public ActionResult CompanyRegister() => View();
+
+    }
+}
