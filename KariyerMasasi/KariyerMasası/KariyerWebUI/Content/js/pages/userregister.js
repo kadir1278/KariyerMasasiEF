62241c3b9982055b5ee918532a151733062084ac
Ermand Haruni <ermandgs74@gmail.com>
2021-11-30 12:28:20 +0300

    userregister.js update

--- KariyerMasasi/KariyerMasası/KariyerWebUI/Content/js/pages/userregister.js
+++ KariyerMasasi/KariyerMasası/KariyerWebUI/Content/js/pages/userregister.js
@@ -1,51 +1,44 @@
 ﻿$(document).ready(function () {
+    $("#business-area-select").select2({
+        placeholder: "Çalışmak istediğiniz alanlar",
+        tags: true,
+        tokenSeperators: ['/', ',', ',', " "]
+    })
+    $("#department-select").select2({
+        placeholder: "Çalışmak istediğiniz departmanlar",
+        tags: true,
+        tokenSeperators: ['/', ',', ',', " "]
+    });
+    $("#city-select").select2({
+        placeholder: "Çalışmak istediğiniz şehirler",
+        tags: true,
+        tokenSeperators: ['/', ',', ',', " "],
+        createTag: function (params) {
+            return undefined;
+        },
+        
+    });
 
-    var ozelDurumContent = '<div class="col-md-12" id="ozelDurum"><div class="form-group">'
-        + '<div class="form-group">'
-        + '<select class="form-control" name="disabled">'
-        + '<option value="Görme Engelli">Görme Engelli</option>  '
-        + ' </select>'
-        + '     </div>'
-        + '</div>';
-    $("#type-select").on('change', function () {      
-        if (this.value != 'Rehber' && !($("#business-area").html() && $("#department").html() && $("#city").html())) {
-            $.ajax({
-                url: '/kullanici-tip',
-                type: 'GET',
-                success: function (result) {
-                    
-                    $("#type").after(result);
-                    if ($("#type-select").val() == 'Özel Durum') {
-                        $("#disabled").removeAttr('hidden');
-                    }
-                    $("#business-area-select").select2({
-                        placeholder: "Çalışmak istediğiniz alanlar",
-                        tags: true,
-                        tokenSeperators: ['/', ',', ',', " "]
-                    })
-                    $("#department-select").select2({
-                        placeholder: "Çalışmak istediğiniz departmanlar",
-                        tags: true,
-                        tokenSeperators: ['/', ',', ',', " "]
-                    });
-                    $("#city-select").select2({
-                        placeholder: "Çalışmak istediğiniz şehirler",
-                        tags: true,
-                        tokenSeperators: ['/', ',', ',', " "],
-                        createTag: function (params) {
-                            return undefined;
-                        }
-                    });
-                }
-            });
-            
-               
+    $("#type-select").on('change', function () {
+        if (this.value != 'Rehber') {            
+            if ($("#type-select").val() == 'Özel Durum') {
+                $("#special-type").show();
+            }
+            else {
+                $("#special-type").hide();
+            }
+            $("#business-area").show();
+            $("#department").show();
+            $("#city").show();
+            $(".select2-container--default").css('width','100%')
+            $(".select2-search__field").css('width','547.6px')
             
         }
-        else if (this.value == 'Rehber') {
-            $("#business-area").remove();
-            $("#department").remove()
-            $("#city").remove()
+        else {
+            $("#business-area").hide();
+            $("#department").hide();
+            $("#city").hide();
+            $("#special-type").hide();
         }
     });
 
