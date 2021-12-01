c0616fc559c121e2119ec8811bf8873ca4a4540a
Ermand Haruni <ermandgs74@gmail.com>
2021-12-01 23:32:38 +0300

    Kayıtlı özgeçmişler sayfası eklendi

---
+++ KariyerMasasi/KariyerMasası/KariyerWebUI/Controllers/MeetingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerWebUI.Controllers
+{
+    public class MeetingController : Controller
+    {
+        [Route("toplantilar"),HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
