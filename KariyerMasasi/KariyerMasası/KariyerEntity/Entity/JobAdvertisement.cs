87cea4c6e6ba03b9d108f3f3f87bcd6aa521011d
bnymnclskn01 <83185397+bnymnclskn01@users.noreply.github.com>
2021-12-19 23:51:04 +0300

    Düzenleme Alındı

--- KariyerMasasi/KariyerMasası/KariyerEntity/Entity/JobAdvertisement.cs
+++ KariyerMasasi/KariyerMasası/KariyerEntity/Entity/JobAdvertisement.cs
@@ -19,8 +19,8 @@ namespace KariyerEntity.Entity
         public string ContractType { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime FinishDate { get; set; }
-        public bool Guide { get; set; }
-        public bool SpecialCase { get; set; }
+        public bool Guide { get; set; } // Kalksın
+        public bool SpecialCase { get; set; } // Özel Durumlumu Başvurabilsin mi ?
         public int CompanyID { get; set; }
         public Company Company { get; set; }
         public int BusinessAreaID { get; set; }
